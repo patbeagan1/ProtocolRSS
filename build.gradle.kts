749d484e8154e8679cef67bc8060a7693ac0f696
patbeagan1 <pbeagan1@gmail.com>
2022-05-18 22:57:10 -0500

    library is working via maven local

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,10 +4,20 @@ plugins {
     kotlin("jvm") version "1.6.21"
     id("org.jlleitschuh.gradle.ktlint") version "10.3.0"
     application
+    `java-library`
+    `maven-publish`
 }
 
-group = "dev.patbeagan"
-version = "1.0-SNAPSHOT"
+group = "dev.patbeagan1"
+version = "0.3-SNAPSHOT"
+
+java {
+    withJavadocJar()
+    withSourcesJar()
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(11))
+    }
+}
 
 repositories {
     mavenCentral()
@@ -15,7 +25,7 @@ repositories {
 
 dependencies {
     testImplementation(kotlin("test"))
-    implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-xml:2.13.3")
+    api("com.fasterxml.jackson.dataformat:jackson-dataformat-xml:2.13.3")
     // https://mvnrepository.com/artifact/org.junit.jupiter/junit-jupiter
     testImplementation("org.junit.jupiter:junit-jupiter:5.8.2")
 }
@@ -30,4 +40,42 @@ tasks.withType<KotlinCompile> {
 
 application {
     mainClass.set("MainKt")
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            artifactId = "protocol-rss"
+            from(components["java"])
+            versionMapping {
+                usage("java-api") {
+                    fromResolutionOf("runtimeClasspath")
+                }
+                usage("java-runtime") {
+                    fromResolutionResult()
+                }
+            }
+            pom {
+                name.set("ProtocolRSS")
+                description.set("A kotlin implementation of the RSS protocol. Derived from https://www.rssboard.org/rss-specification")
+                url.set("https://github.com/patbeagan1/ProtocolRSS")
+                licenses {
+                    license {
+                        name.set("The MIT License (MIT)")
+                        url.set("https://mit-license.org/")
+                    }
+                }
+                developers {
+                    developer {
+                        id.set("patbeagan1")
+                    }
+                }
+                scm {
+                    connection.set("scm:git:git://git@github.com:patbeagan1/ProtocolRSS.git")
+                    developerConnection.set("scm:git:ssh://git@github.com:patbeagan1/ProtocolRSS.git")
+                    url.set("https://github.com/patbeagan1/ProtocolRSS")
+                }
+            }
+        }
+    }
 }
