e8b233231592dbc6383ac260eb909d835b2beddd
patbeagan1 <pbeagan1@gmail.com>
2022-05-20 21:14:06 -0500

    adding in a test coverage numbers as a blocker for the check task

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,20 +5,15 @@ plugins {
     id("org.jlleitschuh.gradle.ktlint") version "10.3.0"
     id("org.jetbrains.kotlinx.binary-compatibility-validator") version "0.10.0"
     application
+    jacoco
     `java-library`
     `maven-publish`
 }
 
+// /////////////////////////
 group = "dev.patbeagan1"
 version = "0.3-SNAPSHOT"
-
-java {
-    withJavadocJar()
-    withSourcesJar()
-    toolchain {
-        languageVersion.set(JavaLanguageVersion.of(11))
-    }
-}
+// /////////////////////////
 
 repositories {
     mavenCentral()
@@ -31,16 +26,64 @@ dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter:5.8.2")
 }
 
+java {
+    withJavadocJar()
+    withSourcesJar()
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(11))
+    }
+}
+
+application {
+    mainClass.set("MainKt")
+}
+
+jacoco {
+    toolVersion = "0.8.7"
+    reportsDirectory.set(layout.buildDirectory.dir("customJacocoReportDir"))
+}
+
 tasks.test {
     useJUnitPlatform()
+}
+
+tasks.check {
+    dependsOn(tasks.jacocoTestCoverageVerification)
 }
 
 tasks.withType<KotlinCompile> {
     kotlinOptions.jvmTarget = "1.8"
 }
 
-application {
-    mainClass.set("MainKt")
+tasks.jacocoTestReport {
+    dependsOn(tasks.test)
+    reports {
+        xml.required.set(false)
+        csv.required.set(false)
+        html.outputLocation.set(layout.buildDirectory.dir("jacocoHtml"))
+    }
+}
+
+tasks.jacocoTestCoverageVerification {
+    violationRules {
+        rule {
+            limit {
+                minimum = "0.80".toBigDecimal()
+            }
+        }
+
+        rule {
+            isEnabled = false
+            element = "CLASS"
+            includes = listOf("org.gradle.*")
+
+            limit {
+                counter = "LINE"
+                value = "TOTALCOUNT"
+                maximum = "0.3".toBigDecimal()
+            }
+        }
+    }
 }
 
 publishing {
--- scripts/format.sh
+++ scripts/format.sh
@@ -1,3 +1,1 @@
-cd ..
-
 ./gradlew ktlintFormat
